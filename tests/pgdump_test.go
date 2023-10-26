ade77f889f57648c9ec1a6c8b305eb918a1bf2fa
Vrutik <vrutikprajapati2809@gmail.com>
2023-10-26 16:49:18 +0530

    feat: add tests for `pgdump`

--- tests/pgdump_test.go
+++ tests/pgdump_test.go
@@ -3,11 +3,13 @@ package tests
 import (
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/vrutik2809/dbdump/utils"
 	"github.com/vrutik2809/dbdump/utils/postgresql"
 )
 
@@ -39,7 +41,7 @@ func (suite *PgDumpTestSuite) SetupSuite() {
 			"name":        "photos",
 			"createQuery": "CREATE TABLE photos (id INT NOT NULL, url VARCHAR (50) NOT NULL, user_id INT NOT NULL);",
 			"insertQuery": "INSERT INTO photos (id, url, user_id) VALUES (1, 'https://user1.com', 1), (2, 'https://user2.com', 2), (3, 'https://user3.com', 3);",
-			"dropQuery":   "DROP TABLE photos;",	
+			"dropQuery":   "DROP TABLE photos;",
 		},
 		map[string]interface{}{
 			"name":        "profiles",
@@ -84,8 +86,113 @@ func (suite *PgDumpTestSuite) TestJsonDump() {
 	assert.NoError(suite.T(), err, "dump directory not created")
 
 	for _, table := range suite.tables {
-		_, err = os.Stat(dumpDir + table["name"].(string) + ext)
-		assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+table["name"].(string)+ext)
+		tableName := table["name"].(string)
+		_, err = os.Stat(dumpDir + tableName + ext)
+		assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+tableName+ext)
+	}
+}
+
+func (suite *PgDumpTestSuite) TestCSVDump() {
+	dumpDir := "../dump/"
+	ext := ".csv"
+	cmd := exec.Command("go", "run", "../main.go", "pg", "-u", "admin", "--password", "admin123", "--host", "localhost", "-p", "5433", "-d", "test", "--dir", dumpDir, "-o", "csv")
+	err := cmd.Run()
+	assert.NoError(suite.T(), err, "pg dump failed")
+
+	_, err = os.Stat(dumpDir)
+	assert.NoError(suite.T(), err, "dump directory not created")
+
+	for _, table := range suite.tables {
+		tableName := table["name"].(string)
+		_, err = os.Stat(dumpDir + tableName + ext)
+		assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+tableName+ext)
+	}
+}
+
+func (suite *PgDumpTestSuite) TestTSVDump() {
+	dumpDir := "../dump/"
+	ext := ".tsv"
+	cmd := exec.Command("go", "run", "../main.go", "pg", "-u", "admin", "--password", "admin123", "--host", "localhost", "-p", "5433", "-d", "test", "--dir", dumpDir, "-o", "tsv")
+	err := cmd.Run()
+	assert.NoError(suite.T(), err, "pg dump failed")
+
+	_, err = os.Stat(dumpDir)
+	assert.NoError(suite.T(), err, "dump directory not created")
+
+	for _, table := range suite.tables {
+		tableName := table["name"].(string)
+		_, err = os.Stat(dumpDir + tableName + ext)
+		assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+tableName+ext)
+	}
+}
+
+func (suite *PgDumpTestSuite) TestTablesDump() {
+	dumpDir := "../dump/"
+	ext := ".json"
+	dumpTables := []string{"users", "photos"}
+	cmd := exec.Command("go", "run", "../main.go", "pg", "-u", "admin", "--password", "admin123", "--host", "localhost", "-p", "5433", "-d", "test", "--dir", dumpDir, "-t", strings.Join(dumpTables, ","))
+	err := cmd.Run()
+	assert.NoError(suite.T(), err, "pg dump failed")
+
+	_, err = os.Stat(dumpDir)
+	assert.NoError(suite.T(), err, "dump directory not created")
+
+	for _, table := range suite.tables {
+		tableName := table["name"].(string)
+		if utils.Contains(dumpTables, tableName) {
+			_, err = os.Stat(dumpDir + tableName + ext)
+			assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+tableName+ext)
+		} else {
+			_, err = os.Stat(dumpDir + tableName + ext)
+			assert.Error(suite.T(), err, "dump directory contains file with name: "+tableName+ext)
+		}
+	}
+}
+
+func (suite *PgDumpTestSuite) TestExcludeTablesDump() {
+	dumpDir := "../dump/"
+	ext := ".json"
+	excludeTables := []string{"photos"}
+	cmd := exec.Command("go", "run", "../main.go", "pg", "-u", "admin", "--password", "admin123", "--host", "localhost", "-p", "5433", "-d", "test", "--dir", dumpDir, "-e", strings.Join(excludeTables, ","))
+	err := cmd.Run()
+	assert.NoError(suite.T(), err, "pg dump failed")
+
+	_, err = os.Stat(dumpDir)
+	assert.NoError(suite.T(), err, "dump directory not created")
+
+	for _, table := range suite.tables {
+		tableName := table["name"].(string)
+		if utils.Contains(excludeTables, tableName) {
+			_, err = os.Stat(dumpDir + tableName + ext)
+			assert.Error(suite.T(), err, "dump directory contains file with name: "+tableName+ext)
+		} else {
+			_, err = os.Stat(dumpDir + tableName + ext)
+			assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+tableName+ext)
+		}
+	}
+}
+
+func (suite *PgDumpTestSuite) TestAggregatedDump() {
+	dumpDir := "../dump/"
+	ext := ".json"
+	dumpTables := []string{"users", "photos"}
+	excludeTables := []string{"photos"}
+	cmd := exec.Command("go", "run", "../main.go", "pg", "-u", "admin", "--password", "admin123", "--host", "localhost", "-p", "5433", "-d", "test", "--dir", dumpDir, "-t", strings.Join(dumpTables, ","), "-e", strings.Join(excludeTables, ","))
+	err := cmd.Run()
+	assert.NoError(suite.T(), err, "pg dump failed")
+
+	_, err = os.Stat(dumpDir)
+	assert.NoError(suite.T(), err, "dump directory not created")
+
+	for _, table := range suite.tables {
+		tableName := table["name"].(string)
+		if utils.Contains(dumpTables, tableName) && !utils.Contains(excludeTables, tableName) {
+			_, err = os.Stat(dumpDir + tableName + ext)
+			assert.NoError(suite.T(), err, "dump directory does not contain file with name: "+tableName+ext)
+		} else {
+			_, err = os.Stat(dumpDir + tableName + ext)
+			assert.Error(suite.T(), err, "dump directory contains file with name: "+tableName+ext)
+		}
 	}
 }
 
